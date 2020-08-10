050e9643e6dc3bada9232468a09c0b5f8a32338b
Loco-doco <55223051+Loco-doco@users.noreply.github.com>
2020-08-10 20:13:43 +0900

    install graphql server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
-console.log("Hello worldd")
+require("dotenv").config()
+import { GraphQLServer} from "graphql-yoga"
+
+const PORT = process.env.PORT || 4000;
+
+const typeDefs = `
+    type Query{
+        hello: String!
+    }
+`
+
+const resolvers = {
+    Query : {
+        hello : () => "Hi"
+    }
+};
+
+const server = new GraphQLServer({
+    typeDefs, resolvers
+});
+console.log("Hello World")
+server.start({port: PORT}, 
+    () => console.log(`Server running on port ${PORT}`)
+);
